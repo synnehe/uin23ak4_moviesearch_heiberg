13f7ef47db74b5974ea020de7187ef5b4b58f2e0
Synnehe <synnehei@hotmail.no>
2023-03-27 22:29:18 +0200

    made page for movie

--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom"
 
 export default function Moviecard({movies}) {
+  //const movie = movies.find((movie) => movie?.movie?.label.replace(/\s/g, '-').toLowerCase() === slug)
+
   return(
     <div id="movie-results">
     {movies?.map((movie, index) => (
@@ -7,7 +10,9 @@ export default function Moviecard({movies}) {
       <article key={index} id="article-card">
         <img src={movie.Poster} alt={movie.Title + " Poster"} placeholder=""/>
         <h2>{movie.Title}, {movie.Year}</h2>
-        <button>Mer om filmen</button>
+        <Link key={index} to={movie?.movie?.Id.replace(/\s/g, "-").toLowerCase()}>
+          <button>Mer om filmen</button>
+        </Link>
       </article>
     </section>
     ))}
